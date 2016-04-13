cdb9ec5fe7ac5d01fc6040d0749cb6e53b289022
Nikita Ignatov <hi@nikitaignatov.com>
2016-04-13 02:30:43 +0200

    initial prototype

---
+++ src/BaconTime.Terminal/Command.cs
@@ -0,0 +1,8 @@
+namespace BaconTime.Terminal
+{
+    enum Command
+    {
+        Assgined = 1,
+        TotalToday = 2
+    }
+}
---
+++ src/BaconTime.Terminal/Configuration.cs
@@ -0,0 +1,9 @@
+namespace BaconTime.Terminal
+{
+    class Configuration
+    {
+        public string username { get; set; }
+        public string key { get; set; }
+        public string endpoint { get; set; }
+    }
+}
