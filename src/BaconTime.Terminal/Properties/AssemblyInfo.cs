79836689aebbc5c0ad558e894c45619ca25e3fa3
Nikita Ignatov <hi@nikitaignatov.com>
2016-04-21 09:04:48 +0200

    changed assembly name

--- src/BaconTime.Terminal/Properties/AssemblyInfo.cs
+++ src/BaconTime.Terminal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BaconTime.Terminal")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MagicTimes")]
+[assembly: AssemblyDescription("Gemini time logging application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaconTime.Terminal")]
+[assembly: AssemblyProduct("MagicTimes")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
